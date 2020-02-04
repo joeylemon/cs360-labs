5c0ff855bbe257ffc4aea8a53ce173c489762f93
joeylemon <joeyclemon@gmail.com>
2020-02-04 17:54:47 -0500

    add better comments and file header

--- lab2/submit/data_uncompress.c
+++ lab2/submit/data_uncompress.c
@@ -1,89 +1,58 @@
-/* Lab 1 -- Fields/dlist/rbtree primer
-   COSC360
-   This program reads data from a binary file with a
-   nasty format and prints it out cleanly
-   Joey Lemon
-   2020-02-09 */
+/**
+ * Lab 2 -- Reading nasty data
+ * COSC360
+ * Joey Lemon
+ * Feb 9, 2020
+ * 
+ * This program reads data from a binary file with a
+ * "compressed" format and prints out its data
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
-    Returns the next number of elements to read
-    Errors:
-        when the n value couldn't be read
-**/
-int get_next_n() {
-    int c = getchar();
-
-    if (c == EOF) {
-        fprintf(stderr, "Input error: no size\n");
-        exit(1);
-    }
-
-    return c;
-}
+ * Gets the next number of elements to read
+ * This will exit the program if the end of stdin is reached
+ * 
+ * @return a positive integer representing how many elements to read
+ */
+int get_next_n();
 
 /**
-    Returns the next number of chars to read for a string
-    Errors:
-        when the size value couldn't be read
-**/
-int get_next_size() {
-    int size = getchar();
-
-    if (size == EOF) {
-        fprintf(stderr, "Input error: no string size\n");
-        exit(1);
-    }
-
-    return size;
-}
+ * Get the size (# of chars) of the upcoming string
+ * This will exit the program if the end of stdin is reached
+ * 
+ * @return a positive integer representing how many chars to read
+ */
+int get_next_size();
 
 /**
-    Reads the next n integers into the buffer
-    Errors:
-        when less than n integers were read
-**/
-void read_next_ints(int *intbuf, int n) {
-    int nread = fread(intbuf, sizeof(int), n + 1, stdin);
-
-    if (nread != (n + 1)) {
-        fprintf(stderr, "Input error: not enough ints\n");
-        exit(1);
-    }
-}
+ * Read the next (n + 1) integers from stdin into the buffer
+ * This will exit the program if <(n + 1) integers were able to be read
+ * 
+ * @param intbuf A buffer to read integers into
+ * @param n The number of integers to read from stdin
+ */
+void read_next_ints(int *intbuf, int n);
 
 /**
-    Reads the next n doubles into the buffer
-    Errors:
-        when less than n doubles were read
-**/
-void read_next_doubles(double *doublebuf, int n) {
-    int nread = fread(doublebuf, sizeof(double), n + 1, stdin);
-
-    if (nread != (n + 1)) {
-        fprintf(stderr, "Input error: not enough doubles\n");
-        exit(1);
-    }
-}
+ * Read the next (n + 1) doubles from stdin into the buffer
+ * This will exit the program if <(n + 1) doubles were able to be read
+ * 
+ * @param intbuf A buffer to read doubles into
+ * @param n The number of doubles to read from stdin
+ */
+void read_next_doubles(double *doublebuf, int n);
 
 /**
-    Reads the next s_size chars into the buffer
-    Errors:
-        when less than s_size chars were read
-**/
-void read_next_string(char *charbuf, int s_size) {
-    int nread = fread(charbuf, sizeof(char), s_size + 1, stdin);
-
-    if (nread != (s_size + 1)) {
-        fprintf(stderr, "Input error: not enough chars\n");
-        exit(1);
-    }
-
-    /* Add null terminator */
-    charbuf[s_size + 1] = '\0';
-}
+ * Read the next (s_size + 1) chars from stdin into the string
+ * This will exit the program if <(s_size + 1) chars were able to be read
+ * 
+ * @param charbuf A char buffer to read characters into
+ * @param n The number of chars to read from stdin
+ */
+void read_next_string(char *charbuf, int s_size);
 
 int main() {
     int c, last_c, i, n, s_size;
@@ -143,7 +112,8 @@ int main() {
         } else if (c == 'n') {
             printf("\n");
 
-        /* If we've reached this point, there's a bad type of data */
+        /* If we've reached this point, there's a type of data
+           that isn't in {s, i, d, n}, which is invalid */
         } else {
             fprintf(stderr, "Input error: bad type\n");
             exit(1);
@@ -152,4 +122,56 @@ int main() {
         /* Keep track of the last data type we read */
         last_c = c;
     }
+}
+
+int get_next_n() {
+    int c = getchar();
+
+    if (c == EOF) {
+        fprintf(stderr, "Input error: no size\n");
+        exit(1);
+    }
+
+    return c;
+}
+
+int get_next_size() {
+    int size = getchar();
+
+    if (size == EOF) {
+        fprintf(stderr, "Input error: no string size\n");
+        exit(1);
+    }
+
+    return size;
+}
+
+void read_next_ints(int *intbuf, int n) {
+    int nread = fread(intbuf, sizeof(int), n + 1, stdin);
+
+    if (nread != (n + 1)) {
+        fprintf(stderr, "Input error: not enough ints\n");
+        exit(1);
+    }
+}
+
+void read_next_doubles(double *doublebuf, int n) {
+    int nread = fread(doublebuf, sizeof(double), n + 1, stdin);
+
+    if (nread != (n + 1)) {
+        fprintf(stderr, "Input error: not enough doubles\n");
+        exit(1);
+    }
+}
+
+void read_next_string(char *charbuf, int s_size) {
+    int nread = fread(charbuf, sizeof(char), s_size + 1, stdin);
+
+    if (nread != (s_size + 1)) {
+        fprintf(stderr, "Input error: not enough chars\n");
+        exit(1);
+    }
+
+    /* Add null terminator */
+    charbuf[s_size + 1] = '\0';
 }
