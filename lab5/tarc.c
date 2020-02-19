e1fef743e700df9170dc1567597604f76184fa61
Joey Christopher Lemon <jlemon3@hydra17.eecs.utk.edu>
2020-02-19 17:42:26 -0500

    relative paths

--- lab5/tarc.c
+++ lab5/tarc.c
@@ -27,56 +27,62 @@ char* get_dirname(char* dir) {
     return last;
 }
 
-void print_file(char* curfile, JRB inodes) {
+int print_file(char* longname, char* shortname, JRB inodes) {
     struct stat statbuf;
-    if (stat(curfile, &statbuf) == -1) {
-        fprintf(stderr, "%s: No such file or directory\n", curfile);
-        return;
+    if (stat(longname, &statbuf) == -1) {
+        fprintf(stderr, "%s: No such file or directory\n", longname);
+        return 1;
     } else if (S_ISLNK(statbuf.st_mode)) {
-        return;
+        return 1;
     }
 
-    printf("0x%08x %-20s 0x%016x ", strlen(curfile), curfile, statbuf.st_ino);
+    printf("0x%08x %-20s 0x%016x ", strlen(shortname), shortname, statbuf.st_ino);
 
     if (jrb_find_int(inodes, statbuf.st_ino) != NULL)
-        return;
-    jrb_insert_int(inodes, statbuf.st_ino, new_jval_s(curfile));
+        return 0;
+    jrb_insert_int(inodes, statbuf.st_ino, new_jval_s(longname));
 
     printf("0x%08x 0x%016x ", (int)statbuf.st_mode, statbuf.st_mtime);
 
     if(S_ISDIR(statbuf.st_mode)){
-        return;
+        return 0;
     }
 
     printf("0x%016x bytes", statbuf.st_size);
+    return 0;
 }
 
-void iterate_dir(char* dir, int root, JRB inodes) {
+void iterate_dir(char* fullpath, char* shortpath, int root, JRB inodes) {
     struct dirent *dp;
     DIR *dfd;
 
-    if ((dfd = opendir(dir)) == NULL) {
-        fprintf(stderr, "%s: No such file or directory\n", dir);
+    if ((dfd = opendir(fullpath)) == NULL) {
+        fprintf(stderr, "%s: No such file or directory\n", fullpath);
         return;
     }
 
-    char curfile[256];
+    char longname[256];
+    char shortname[256];
     while ((dp = readdir(dfd)) != NULL) {
         /* Skip ./ and ../ */
         if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
             continue;
 
-        sprintf(curfile, "%s/%s", dir, dp->d_name);
+        sprintf(longname, "%s/%s", fullpath, dp->d_name);
+        sprintf(shortname, "%s/%s", shortpath, dp->d_name);
 
-        print_file(curfile, inodes);
+        print_file(longname, shortname, inodes);
         printf("\n");
 
         struct stat statbuf;
-        if (stat(curfile, &statbuf) == -1)
+        if (stat(longname, &statbuf) == -1)
             continue;
 
-        if(S_ISDIR(statbuf.st_mode))
-            iterate_dir(curfile, 0, inodes);
+        if(S_ISDIR(statbuf.st_mode)) {
+            char dir_name[256];
+            strcpy(dir_name, shortname);
+            iterate_dir(longname, dir_name, 0, inodes);
+        }
     }
 }
 
@@ -89,7 +95,12 @@ int main(int argc, char **argv) {
     printf("%-10s %-20s %-18s %-10s %-18s %-18s\n", "name size", "name", "inode", "mode", "mod time", "file size");
 
     JRB inodes = make_jrb();
-    print_file(argv[1], inodes);
+    char dir_suffix[256];
+    strcpy(dir_suffix, get_dirname(strdup(argv[1])));
+
+    if (print_file(argv[1], dir_suffix, inodes) != 0)
+        exit(1);
     printf("\n");
-    iterate_dir(argv[1], 1, inodes);
+    
+    iterate_dir(argv[1], dir_suffix, 1, inodes);
 }
